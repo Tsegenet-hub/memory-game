09d217f2db53976352997dd2c70b35f8d6fe393b
Tsegenet-hub <55013639+Tsegenet-hub@users.noreply.github.com>
2020-01-06 14:24:54 -0500

    deploy

---
+++ build/precache-manifest.50c82c00b0923ddbb0a56cd213ceb04c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e897f1481b72feb1b8dfa72e908de5e",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "1ef82df970b698cda512",
+    "url": "/memory-game/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "7614a5d7f7925428d83f",
+    "url": "/memory-game/static/css/main.e5c3751c.chunk.css"
+  },
+  {
+    "revision": "1ef82df970b698cda512",
+    "url": "/memory-game/static/js/2.9ea80a51.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/memory-game/static/js/2.9ea80a51.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7614a5d7f7925428d83f",
+    "url": "/memory-game/static/js/main.9531cf85.chunk.js"
+  },
+  {
+    "revision": "c3e29c6f82ed5623f2d2",
+    "url": "/memory-game/static/js/runtime-main.ac160a44.js"
+  },
+  {
+    "revision": "1379f89e77cc3fe6c824a911e05aba7a",
+    "url": "/memory-game/static/media/Ben.1379f89e.png"
+  },
+  {
+    "revision": "85ec9a6b109da62181cf66c8b9c50a66",
+    "url": "/memory-game/static/media/Janice.85ec9a6b.jpg"
+  },
+  {
+    "revision": "615bec348661164764339a440c4d3098",
+    "url": "/memory-game/static/media/Joey.615bec34.jpg"
+  },
+  {
+    "revision": "297d66040e868964b8e96442abf14559",
+    "url": "/memory-game/static/media/Joshua.297d6604.png"
+  },
+  {
+    "revision": "6a63e3894864f9a51fedcd62c0624902",
+    "url": "/memory-game/static/media/Julie.6a63e389.png"
+  },
+  {
+    "revision": "c59d0946c6d6c4746db0aafa16c03d70",
+    "url": "/memory-game/static/media/Mindy.c59d0946.jpg"
+  },
+  {
+    "revision": "7468e84fad9953b35b0f595064ff0b62",
+    "url": "/memory-game/static/media/Phoebe.7468e84f.jpg"
+  },
+  {
+    "revision": "99c0c82fc93583fb5e4f1539cdb679ca",
+    "url": "/memory-game/static/media/Rachel.99c0c82f.png"
+  },
+  {
+    "revision": "2b910fc7bd70758214ab5ca28b5ae734",
+    "url": "/memory-game/static/media/Richard.2b910fc7.jpg"
+  }
+]);
