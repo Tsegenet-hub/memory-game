e84f17f78e9ebb6de94d38af46043efb4ce99b2f
Tsegenet-hub <55013639+Tsegenet-hub@users.noreply.github.com>
2020-01-06 14:20:14 -0500

    Updates

---
+++ precache-manifest.f9ad561c9c9bc6a18dfd0cbe3fea2191.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f8ba098cade022a7b5ca69e6619847",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ef82df970b698cda512",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "7614a5d7f7925428d83f",
+    "url": "/static/css/main.e5c3751c.chunk.css"
+  },
+  {
+    "revision": "1ef82df970b698cda512",
+    "url": "/static/js/2.9ea80a51.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9ea80a51.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7614a5d7f7925428d83f",
+    "url": "/static/js/main.9531cf85.chunk.js"
+  },
+  {
+    "revision": "ed85b00e9ccaf58e79cc",
+    "url": "/static/js/runtime-main.5aa5f7ca.js"
+  },
+  {
+    "revision": "1379f89e77cc3fe6c824a911e05aba7a",
+    "url": "/static/media/Ben.1379f89e.png"
+  },
+  {
+    "revision": "85ec9a6b109da62181cf66c8b9c50a66",
+    "url": "/static/media/Janice.85ec9a6b.jpg"
+  },
+  {
+    "revision": "615bec348661164764339a440c4d3098",
+    "url": "/static/media/Joey.615bec34.jpg"
+  },
+  {
+    "revision": "297d66040e868964b8e96442abf14559",
+    "url": "/static/media/Joshua.297d6604.png"
+  },
+  {
+    "revision": "6a63e3894864f9a51fedcd62c0624902",
+    "url": "/static/media/Julie.6a63e389.png"
+  },
+  {
+    "revision": "c59d0946c6d6c4746db0aafa16c03d70",
+    "url": "/static/media/Mindy.c59d0946.jpg"
+  },
+  {
+    "revision": "7468e84fad9953b35b0f595064ff0b62",
+    "url": "/static/media/Phoebe.7468e84f.jpg"
+  },
+  {
+    "revision": "99c0c82fc93583fb5e4f1539cdb679ca",
+    "url": "/static/media/Rachel.99c0c82f.png"
+  },
+  {
+    "revision": "2b910fc7bd70758214ab5ca28b5ae734",
+    "url": "/static/media/Richard.2b910fc7.jpg"
+  }
+]);
